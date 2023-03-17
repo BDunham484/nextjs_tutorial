4bcee3454ba810e2b8d7a796f498ef0e2fd87ad8
ThereWolf <bdunham484@gmail.com>
2023-03-17 18:44:25 -0500

    add css

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,19 +1,34 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
+import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
         <>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2>
-            <Image
-            src="/images/i'm-confused.jpg"
-            height={400}
-            width={400}
-            alt="I'm Confused"
-            />
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                />
+                <h1>First Post</h1>
+                <h2>
+                    <Link href="/">Back to home</Link>
+                </h2>
+                <Image
+                    src="/images/i'm-confused.jpg"
+                    height={400}
+                    width={400}
+                    alt="I'm Confused"
+                />
+            </Layout>
         </>
-    )
+    );
 }
